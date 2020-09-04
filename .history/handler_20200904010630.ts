57a23936d069565f42a6ec8f648e4f77996aba19
Kavin Jey <kavinjey@benchedms.ca>
2020-09-04 04:32:03 -0400

    removing files

--- .history/handler_20200904010630.ts
+++
@@ -1,6 +0,0 @@
-import "source-map-support/register";
-
-export const hello = (request, response) => {
-  console.log(request);
-  response.status(200).send("Hello World! Let's start Typescript!!");
-};
--- .history/handler_20200904012241.ts
+++
@@ -1,7 +0,0 @@
-import "source-map-support/register";
-import { HttpFunction } from "@google-cloud/functions-framework/build/src/functions";
-
-export const hello: HttpFunction = (request, response) => {
-  console.log(request);
-  response.status(200).send("Hello World! Let's start Typescript!!");
-};
